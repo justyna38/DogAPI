c78b711825463940301889e3fe8a50c40fd94a85
justyna38 <warzecha.justyna93@gmail.com>
2024-03-22 15:32:14 +0100

    my first commit

---
+++ DogAPI/DogAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogAPIApp.swift
+//  DogAPI
+//
+//  Created by Justyna Warzecha on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DogAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
