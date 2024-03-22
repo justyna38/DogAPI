a4b722cae8c988fc7696caa1b8a527f160200242
justyna38 <warzecha.justyna93@gmail.com>
2024-03-22 16:32:18 +0100

    merge commit

---
+++ DogAPI/Model/ViewModel/DogViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DogViewModel.swift
+//  DogAPI
+//
+//  Created by Justyna Warzecha on 22/03/2024.
+//
+
+import Foundation
---
+++ DogAPI/Model/ViewModel/View/DogView.swift
@@ -0,0 +1,18 @@
+//
+//  DogView.swift
+//  DogAPI
+//
+//  Created by Justyna Warzecha on 22/03/2024.
+//
+
+import SwiftUI
+
+struct DogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DogView()
+}
